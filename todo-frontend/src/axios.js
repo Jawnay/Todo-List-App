1fcaf3dcb691bc6229591c4da544f7d5eef6d2f3
Jonathan Ly <Jly2003@gmail.com>
2024-02-07 00:44:10 -0800

    added base url

--- todo-frontend/src/axios.js
+++ todo-frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "https://mern-todolist-app-6lne.onrender.com/",
 })
 
 export default instance
